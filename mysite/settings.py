20837941b698c899a1ba42f063957d753177e36a
Bohdan Kuzik <bogdankuzik2509@gmail.com>
2025-03-19 17:32:25 +0100

    some changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -126,6 +126,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'kuzikbv2509@gmail.com'
-EMAIL_HOST_PASSWORD = 'icfu arxc opfc bfih'
+EMAIL_HOST_PASSWORD = 'xxxx xxxx xxxx xxxx'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
